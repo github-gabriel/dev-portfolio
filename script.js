cedc9b79560e687859aa146f15fa22df00d0a261
Gabriel <92476790+github-gabriel@users.noreply.github.com>
2023-12-26 16:18:28 +0100

    Default background-color for buttons

--- script.js
+++ script.js
@@ -1,52 +1,56 @@
 // Ein Großteil des Scripts habe ich von: https://www.w3schools.com/howto/howto_js_tabs.asp
 
-function onLoadFunctions(){
-    document.getElementById("codeBtn").classList.add('focused');
-    document.getElementsByClassName("run")[0].addEventListener("click", function (){
-        openTab("Console")
-    })
-    document.getElementsByClassName("stop")[0].addEventListener("click", function (){
-        openTab("Code")
-    })
-    document.getElementById("codeBtn").addEventListener("click", function (){
-        openTab("Code")
-    })
-    document.getElementById("consoleBtn").addEventListener("click", function (){
-        openTab("Console")
-    })
+function onLoadFunctions() {
+  document.getElementById("codeBtn").classList.add("focused");
+  document
+    .getElementsByClassName("run")[0]
+    .addEventListener("click", function () {
+      openTab("Console");
+    });
+  document
+    .getElementsByClassName("stop")[0]
+    .addEventListener("click", function () {
+      openTab("Code");
+    });
+  document.getElementById("codeBtn").addEventListener("click", function () {
+    openTab("Code");
+  });
+  document.getElementById("consoleBtn").addEventListener("click", function () {
+    openTab("Console");
+  });
 }
 window.onload = onLoadFunctions;
 
 function openTab(tabName) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const codeBtn = document.getElementById("codeBtn");
-    const consoleBtn = document.getElementById("consoleBtn");
-    const tabContent = document.getElementsByClassName("tabcontent");
-    const tabLinks = document.getElementsByClassName("tablinks");
+  const codeBtn = document.getElementById("codeBtn");
+  const consoleBtn = document.getElementById("consoleBtn");
+  const tabContent = document.getElementsByClassName("tabcontent");
+  const tabLinks = document.getElementsByClassName("tablinks");
 
-    // Reset all tabs and buttons
-    codeBtn.classList.remove('focused');
-    codeBtn.offsetWidth;
-    consoleBtn.classList.remove('focused');
-    consoleBtn.offsetWidth;
+  // Reset all tabs and buttons
+  codeBtn.classList.remove("focused");
+  codeBtn.offsetWidth;
+  consoleBtn.classList.remove("focused");
+  consoleBtn.offsetWidth;
 
-    for (let i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
-    }
+  for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
 
-    for (let i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
-    }
+  for (let i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+  }
 
-    // Show the selected tab and add "active" class to the button
-    document.getElementById(tabName).style.display = "block";
-    let button;
-    if (tabName === "Code") {
-        button = codeBtn;
-    } else {
-        button = consoleBtn;
-    }
-    button.classList.add('focused');
-    void button.offsetWidth;
+  // Show the selected tab and add "active" class to the button
+  document.getElementById(tabName).style.display = "block";
+  let button;
+  if (tabName === "Code") {
+    button = codeBtn;
+  } else {
+    button = consoleBtn;
+  }
+  button.classList.add("focused");
+  void button.offsetWidth;
 }
